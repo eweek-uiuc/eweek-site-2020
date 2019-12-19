9ca605a2915c805c053ed5a1fdb89453f555a751
Lauren Ho <laurenh4@illinois.edu>
2019-12-19 11:47:32 -0600

    About description

--- src/scenes/About/index.jsx
+++ src/scenes/About/index.jsx
@@ -20,6 +20,17 @@ const Member = (props) => {
 
 const About = () => (
     <div className="about">
+      <div className="text-container">
+        <h1>E-WEEK COMMITTEE</h1>
+        <p>This program is organized and run by members of <a href="http://www.ec.illinois.edu" target="_blank" rel="noopener noreferrer">Engineering Council</a>
+          , a student-run umbrella organization of Illinois' engineering societies. We spend the Fall semester putting together
+          this week-long, themed event.
+        </p>
+        <p>Our mission is to honor the creative and practical spirit of engineering.
+          We aim to emphasize the importance of math, science, and teamwork toward transforming
+          the impossible into the probable.
+        </p>
+      </div>
       <div className="container">
         <div className="committee">
           { committee.map(c => <Member {...c} key={c.name} />) }
