579da002e076cce8d71975b1e831a82d0b340c8e
Ramya Nagapudi <ramyasn2@illinois.edu>
2020-01-18 13:12:41 -0600

    Create index.jsx

---
+++ src/scenes/Leaderboard/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Leaderboard = () => (
+    <div className="leaderboard">
+      <div className="text-container">
+        <h1>E-WEEK LEADERBOARDS</h1>
+      </div>
+    </div>
+);
+  
+export default Leaderboard;
