c1278a3dcbf04c4cfa68858847aff8d392514c69
Ramya Nagapudi <ramyasn2@illinois.edu>
2020-01-17 23:36:52 -0600

    Update index.jsx

--- src/scenes/Awards/index.jsx
+++ src/scenes/Awards/index.jsx
@@ -1,242 +1,41 @@
 import React from 'react';
 
-class Awards {
-    render() {
-        return (
-          <div className="awards">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h1>EWEEK <span className="highlight">Awards</span></h1>
-                  <hr />
-                </div>
-              </div>
-              <div className="row text-center">
-                <div className="col-sm-12"><a href="https://docs.google.com/forms/d/e/1FAIpQLSfKkBtRjBsSOLrtPNTmlFOsqbGedyIk2hq6-FeNcNTLvRPZzw/viewform" target="_blank" rel="noopener noreferrer">Vote for your friends for EWEEK Awards!</a></div>
-              </div>
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Cleverest Girl (E-Week Queen)</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt="" /></div>
-                <div className="col-sm-4"><img className="img-rounded" src="https://ih1.redbubble.net/image.215393480.0334/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.lite-1u3.jpg" alt="" /></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-offset-3 col-md-3" />
-                <div className="col-md-3" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>E-Week King</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="https://cdn3.movieweb.com/i/article/TUWd23Dhgxrvc4Tzr1XG9rbESVHKxt/798:50/Jurassic-World-Posters-T-Rex-Indominus.jpg" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-offset-3 col-md-3" />
-                <div className="col-md-2" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Cutest Couple</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="https://www.out.com/sites/out.com/files/2015/06/08/leadimage_jurassicworld.jpg" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-4" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Best Friendship</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="https://theplaylist.net/wp-content/uploads/2016/05/jurassic-world-11.jpg" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-offset-2 col-md-4" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Most E-Week Spirit</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="https://jonathondsvendsen.files.wordpress.com/2018/02/tumblr_n790wudwj51qgxy6bo1_500.jpg" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-offset-2 col-md-4" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Most likely to have most of their shirt unbuttoned</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="https://uproxx.files.wordpress.com/2018/05/jeff-goldblum-jurassic-park.jpg?quality=95&w=650" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-offset-2 col-md-4" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Best dinosaur roar</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="https://www.telegraph.co.uk/content/dam/films/2016/09/20/Jurassic-World-12-xlarge_trans_NvBQzQNjv4BqRp36Ti1MFCYr8PMuS2fHb17hoDUspm84EYl8tHPMRlk.jpg?imwidth=450" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-offset-2 col-md-4" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Most likely not to endorse the park</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="http://jurassicparkbtf.weebly.com/uploads/2/7/8/4/27841479/3442694_orig.png" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Most likely to dress entirely in khaki</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="https://cdn-images-1.medium.com/max/1600/0*d2YJfzr6VPe39rBd" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-offset-2 col-md-4" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Most likely to be a hacker</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="https://thumbs.gfycat.com/ForcefulFemaleDeermouse-size_restricted.gif" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-4" />
-                <div className="col-md-4" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Most likely to be shocked by an electric fence</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="https://thumbs.gfycat.com/DopeyLoathsomeAnglerfish-size_restricted.gif" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-4" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Messiest work space</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="https://cdn.vox-cdn.com/thumbor/sJrY5JmgU0qN54z2XqUa2Cltfms=/0x0:1440x653/1200x800/filters:focal(314x95:544x325)/cdn.vox-cdn.com/uploads/chorus_image/image/60017631/dennis_nedry_1440.0.jpg" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-offset-2 col-md-4" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Most likely to be eaten off a toilet</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="https://i.gifer.com/Ue7r.gif" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-offset-2 col-md-4" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Most likely to spare no expense</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="http://www.jurassic-pedia.com/wp-content/uploads/2011/03/hammond_1.png" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-offset-2 col-md-4" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Most likely to slip in the Italian Beef Zone</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="http://img.sndimg.com/food/image/upload/w_614,h_461,c_fit/v1/img/recipes/30/48/4/xrblDf1TTOWGFKdMakQi_DSC_0421.JPG" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-              </div>
-              <br />
-              <div className="row text-center">
-                <div className="col-md-offset-2 col-md-4" />
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-lg-12 text-center">
-                  <h3>Best E-Week Video</h3>
-                </div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-sm-4"><img className="img-rounded" src="https://akns-images.eonline.com/eol_images/Entire_Site/201553/rs_500x210-150603125232-anigif_enhanced-buzz-11799-1403287509-4.gif?fit=inside|900:auto&output-quality=90" alt=""/></div>
-                <div className="col-sm-4"><img className="img-circle" alt=""/></div>
-                <div className="col-lg-12 text-center">
-                  <h3>Video Submissions:</h3>
-                </div>
-              </div>
-              <br />
-              <div className="row">
-                <div className="col-md-6"><iframe title="myFrame1" width={560} height={315} src="https://www.youtube.com/embed/czo3oTlKXas" frameBorder={0} allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /></div>
-                <div className="col-md-6"><iframe title="myFrame2" width={560} height={315} src="https://www.youtube.com/embed/t6_PbS2oD6o" frameBorder={0} allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /></div>
-                <div className="col-md-6"><iframe title="myFrame3" width={560} height={315} src="https://www.youtube.com/embed/DoPymUu2HPw" frameBorder={0} allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /></div>
-                <div className="col-md-6"><iframe title="myFrame4" width={560} height={315} src="https://www.youtube.com/embed/dYNL0yWN0qw" frameBorder={0} allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /></div>
-                <div className="col-md-6"><iframe title="myFrame5" width={560} height={315} src="https://www.youtube.com/embed/n0lRJojuv20" frameBorder={0} allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /></div>
-                <div className="col-md-6"><iframe title="myFrame6" width={560} height={315} src="https://www.youtube.com/embed/attkxSz_sGs" frameBorder={0} allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /></div>
-                <div className="col-md-6"><iframe title="myFrame7" width={560} height={315} src="https://www.youtube.com/embed/9oyvv7Zyil0" frameBorder={0} allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /></div>
-                <div className="col-md-6"><iframe title="myFrame8" width={560} height={315} src="https://www.youtube.com/embed/ADejCZUsRKU" frameBorder={0} allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /></div>
-                <div className="col-md-6"><iframe title="myFrame9" width={560} height={315} src="https://www.youtube.com/embed/xNnttwaNZUM" frameBorder={0} allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /></div>
-                <div className="col-md-6"><iframe title="myFrame10" width={560} height={315} src="https://www.youtube.com/embed/Cp5XzD8i-L8" frameBorder={0} allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /></div>
-                {/*</div>*/}
-              </div>
-            </div>
-            </div>
-          );
-        }
-    }
+import awardtypes from './awardtypes.js';
 
+const Awardmember = (props) => {
+    const { name, major, year, image } = props;
+    return (
+        <div className="awardmember">
+            <div className="content" style={{ backgroundImage: `url(${image})` }}>
+                <div className="info">
+                <h3>{ name }</h3>
+                <p>{ major }</p>
+                <p>{ year }</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const Award = () => (
+    <div className="awards">
+      <div className="text-container">
+        <h1>E-WEEK AWARDS</h1>
+        <p>This program is organized and run by members of <a href="http://www.ec.illinois.edu" target="_blank" rel="noopener noreferrer">Engineering Council</a>
+          , a student-run umbrella organization of Illinois' engineering societies. We spend the Fall semester putting together
+          this week-long, themed event.
+        </p>
+        <p>Our mission is to honor the creative and practical spirit of engineering.
+          We aim to emphasize the importance of math, science, and teamwork toward transforming
+          the impossible into the probable.
+        </p>
+      </div>
+      <div className="container">
+        <div className="awardtypes">
+          { awardtypes.map(c => <Awardmember {...c} key={c.name} />) }
+        </div>
+      </div>
+    </div>
+);
+  
 export default Awards;
