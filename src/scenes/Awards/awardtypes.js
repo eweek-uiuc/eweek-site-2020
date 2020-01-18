13575d038a5a0d894a33f052fb6d70ef15d2698a
Ramya Nagapudi <ramyasn2@illinois.edu>
2020-01-17 23:35:11 -0600

    Create awardtypes.js

---
+++ src/scenes/Awards/awardtypes.js
@@ -0,0 +1,117 @@
+import Queen from '../../assets/awards/Queen.jpg';
+import King from '../../assets/awards/King.jpg';
+import Couple from '../../assets/awards/Couple.jpg';
+import Friendship from '../../assets/awards/Friendship.jpg';
+import Spirit from '../../assets/awards/Spirit.jpg';
+import Unbutton from '../../assets/awards/Unbutton.jpg';
+import Roar from '../../assets/awards/Roar.jpg';
+import Endorse from '../../assets/awards/Endorse.png';
+import Khaki from '../../assets/awards/Khaki.jpg';
+import Hacker from '../../assets/awards/Hacker.jpg';
+import Fence from '../../assets/awards/Fence.jpg';
+import Messy from '../../assets/awards/Messy.jpg';
+import Eaten from '../../assets/awards/Eaten.jpg';
+import Expense from '../../assets/awards/Expense.png';
+import Slip from '../../assets/awards/Slip.jpeg';
+import Video from '../../assets/awards/Video.gif';
+
+const awardtypes = [
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Queen,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: King,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Couple,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Friendship,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Spirit,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Unbutton,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Roar,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Endorse,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Khaki,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Hacker,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Fence,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Messy,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Eaten,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Expense,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Slip,
+    },
+    {
+      name: '',
+      major: '',
+      year: '',
+      image: Video,
+    }
+  ];
+  
+  export default awardtypes;
