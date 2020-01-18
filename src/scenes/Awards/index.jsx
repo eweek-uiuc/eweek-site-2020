82cf34c1741fc2730c5e0e177b1c19988eb2ddf4
Ramya Nagapudi <ramyasn2@illinois.edu>
2020-01-18 00:10:11 -0600

    Update index.jsx

--- src/scenes/Awards/index.jsx
+++ src/scenes/Awards/index.jsx
@@ -21,7 +21,7 @@ const Awards = () => (
     <div className="awards">
       <div className="text-container">
         <h1>E-WEEK AWARDS</h1>
-        <p>Vote for your friends for EWEEK Awards!
+        <p>Vote for your friends for EWEEK Awards, testing!
         </p>
       </div>
       <div className="container">
