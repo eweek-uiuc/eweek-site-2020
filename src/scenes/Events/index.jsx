2fdb115864b93609a5d77bbc38d96b95effa54c6
Nila <nilakrishnan27@gmail.com>
2020-02-05 13:30:46 -0600

    Start mobile styling and edit events list

--- src/scenes/Events/index.jsx
+++ src/scenes/Events/index.jsx
@@ -12,9 +12,14 @@ const Date = (props) => {
                 </div>
                 <div className="info">
                     <div className='name'>{ e.name }</div>
-                    <div className='time'>{ e.time }</div>
-                    <div className='location'>{ e.location }</div>
-                    <div className='points'>{ e.points }</div>
+                    <div className='when'>
+                        <div className='date'>{ e.date }</div>
+                        <div className='time'>{ e.time }</div>
+                    </div>
+                    <div className='details'>
+                        <div className='location'>{ e.location }</div>
+                        <div className='points'>{ e.points }</div>
+                    </div>
                 </div>
             </div>)}
         </div>
@@ -26,9 +31,9 @@ const Events = () => (
         <div className='sidebar'>
             <div className="links">
                 <a href="https://docs.google.com/document/d/1pjhlVNDJpfb0KZObH8u5VE4wj7Fwvr_i5hlG8mP2DUg/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Captain's Packet</a>
-                <a href='https://eweekillinois.org/events' target="_blank" rel="noopener noreferrer">Event Rules</a>
-                <a href='https://eweekillinois.org/events' target="_blank" rel="noopener noreferrer">Photo Scavenger Hunt</a>
-                <a href='https://eweekillinois.org/events' target="_blank" rel="noopener noreferrer">Co-Host Sign-ups</a>
+                <a href='https://docs.google.com/document/d/1iuW0FC6YjSS1Rmj30V0z7NMVewUHLslWEgQ4geVcql8/edit?usp=sharing' target="_blank" rel="noopener noreferrer">Event Rules</a>
+                <a href='https://docs.google.com/spreadsheets/d/1vs0VFAaVYvXsjeIggXeu57TD6w1eKUntD8NJrMewcUk/edit?usp=sharing' target="_blank" rel="noopener noreferrer">Photo Scavenger Hunt</a>
+                <a href='https://forms.gle/3KeJt2oB4VJ8iGxf6' target="_blank" rel="noopener noreferrer">Co-Host Sign-ups</a>
             </div>
             {/*<div className="dates">*/}
             {/*    { events.map(c => <div className='date'>{c.date}</div>) }*/}
--- src/scenes/Home/index.jsx
+++ src/scenes/Home/index.jsx
@@ -8,7 +8,10 @@ class Home extends Component {
     render() {
         return (
             <div className="home">
-                <img className="logo" src={Logo} alt="logo"/>
+                <div className="start">
+                    <img className="start__logo" src={Logo} alt="logo"/>
+                    <div className="start__dates">{"February 16th - 22nd, 2020"}</div>
+                </div>
                 <About/>
                 <Sponsors/>
             </div>
